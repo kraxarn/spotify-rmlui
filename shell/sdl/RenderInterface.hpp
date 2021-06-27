9192b3af335fe2b99c6a8cb330f39936579f15fa
kraxarn <me@kraxarn.com>
2021-06-27 15:27:11 +0200

    Fix render interface

--- shell/sdl/RenderInterface.hpp
+++ shell/sdl/RenderInterface.hpp
@@ -3,6 +3,7 @@
 #include "RmlUi/Core.h"
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <GL/glew.h>
 
 // Based off
@@ -12,6 +13,7 @@ namespace Sdl
 {
 	class RenderInterface: public Rml::RenderInterface
 	{
+	public:
 		RenderInterface(SDL_Renderer *renderer, SDL_Window *window)
 			: sdl_renderer(renderer),
 			sdl_window(window)
@@ -36,7 +38,7 @@ namespace Sdl
 			float texh = 0.0f;
 
 			SDL_Texture *sdl_texture = nullptr;
-			if (texture)
+			if (texture != 0U)
 			{
 				glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 				sdl_texture = (SDL_Texture *) texture;
@@ -47,7 +49,7 @@ namespace Sdl
 			{
 				Positions[i] = vertices[i].position;
 				Colors[i] = vertices[i].colour;
-				if (sdl_texture)
+				if (sdl_texture != nullptr)
 				{
 					TexCoords[i].x = vertices[i].tex_coord.x * texw;
 					TexCoords[i].y = vertices[i].tex_coord.y * texh;
@@ -71,7 +73,7 @@ namespace Sdl
 			glDisableClientState(GL_VERTEX_ARRAY);
 			glDisableClientState(GL_COLOR_ARRAY);
 
-			if (sdl_texture)
+			if (sdl_texture != nullptr)
 			{
 				SDL_GL_UnbindTexture(sdl_texture);
 				glDisableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -81,8 +83,8 @@ namespace Sdl
 			glPopMatrix();
 			/* Reset blending and draw a fake point just outside the screen to let SDL know that it needs to reset its state in case it wants to render a texture */
 			glDisable(GL_BLEND);
-			SDL_SetRenderDrawBlendMode(mRenderer, SDL_BLENDMODE_NONE);
-			SDL_RenderDrawPoint(mRenderer, -1, -1);
+			SDL_SetRenderDrawBlendMode(sdl_renderer, SDL_BLENDMODE_NONE);
+			SDL_RenderDrawPoint(sdl_renderer, -1, -1);
 		}
 
 		/**
@@ -105,29 +107,30 @@ namespace Sdl
 		 */
 		void SetScissorRegion(int x, int y, int width, int height) override
 		{
-			int w_width, w_height;
-			SDL_GetWindowSize(mScreen, &w_width, &w_height);
+			int w_width;
+			int w_height;
+			SDL_GetWindowSize(sdl_window, &w_width, &w_height);
 			glScissor(x, w_height - (y + height), width, height);
 		}
 
 		/**
 		 * Called by RmlUi when a texture is required by the library.
 		 */
-		bool LoadTexture(Rml::TextureHandle &texture_handle, Rml::Vector2i &texture_dimensions,
-			const Rml::String &source) override
+		auto LoadTexture(Rml::TextureHandle &texture_handle, Rml::Vector2i &texture_dimensions,
+			const Rml::String &source) -> bool override
 		{
 			Rml::FileInterface *file_interface = Rml::GetFileInterface();
 			Rml::FileHandle file_handle = file_interface->Open(source);
-			if (!file_handle)
+			if (file_handle == 0U)
 			{
 				return false;
 			}
 
 			file_interface->Seek(file_handle, 0, SEEK_END);
-			size_t buffer_size = file_interface->Tell(file_handle);
+			auto buffer_size = file_interface->Tell(file_handle);
 			file_interface->Seek(file_handle, 0, SEEK_SET);
 
-			char *buffer = new char[buffer_size];
+			auto *buffer = new char[buffer_size];
 			file_interface->Read(buffer, buffer_size, file_handle);
 			file_interface->Close(file_handle);
 
@@ -142,14 +145,14 @@ namespace Sdl
 
 			Rml::String extension = source.substr(i + 1, source.length() - i);
 
-			SDL_Surface *surface =
-				IMG_LoadTyped_RW(SDL_RWFromMem(buffer, int(buffer_size)), 1, extension.c_str());
+			auto *surface = IMG_LoadTyped_RW(SDL_RWFromMem(buffer, int(buffer_size)),
+				1, extension.c_str());
 
-			if (surface)
+			if (surface != nullptr)
 			{
-				SDL_Texture *texture = SDL_CreateTextureFromSurface(mRenderer, surface);
+				SDL_Texture *texture = SDL_CreateTextureFromSurface(sdl_renderer, surface);
 
-				if (texture)
+				if (texture != nullptr)
 				{
 					texture_handle = (Rml::TextureHandle) texture;
 					texture_dimensions = Rml::Vector2i(surface->w, surface->h);
@@ -170,8 +173,8 @@ namespace Sdl
 		 * Called by RmlUi when a texture is required to be built from an internally-generated
 		 * sequence of pixels.
 		 */
-		bool GenerateTexture(Rml::TextureHandle &texture_handle, const Rml::byte *source,
-			const Rml::Vector2i &source_dimensions) override
+		auto GenerateTexture(Rml::TextureHandle &texture_handle, const Rml::byte *source,
+			const Rml::Vector2i &source_dimensions) -> bool override
 		{
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
 			Uint32 rmask = 0xff000000;
@@ -188,7 +191,7 @@ namespace Sdl
 			SDL_Surface *surface = SDL_CreateRGBSurfaceFrom((void *) source, source_dimensions.x,
 				source_dimensions.y, 32, source_dimensions.x * 4,
 				rmask, gmask, bmask, amask);
-			SDL_Texture *texture = SDL_CreateTextureFromSurface(mRenderer, surface);
+			SDL_Texture *texture = SDL_CreateTextureFromSurface(sdl_renderer, surface);
 			SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
 			SDL_FreeSurface(surface);
 			texture_handle = (Rml::TextureHandle) texture;
@@ -199,7 +202,7 @@ namespace Sdl
 		/**
 		 * Called by RmlUi when a loaded texture is no longer required.
 		 */
-		void ReleaseTexture(Rml::TextureHandle texture) override
+		void ReleaseTexture(Rml::TextureHandle texture_handle) override
 		{
 			SDL_DestroyTexture((SDL_Texture *) texture_handle);
 		}
